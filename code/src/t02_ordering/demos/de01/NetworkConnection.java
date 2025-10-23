d76429d983f7a2b45f2a8e88c6a846cbf3a04ecc
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-10-23 11:48:50 +0100

    Adding improved NetworkConnection

--- code/src/t02_ordering/demos/de01/NetworkConnection.java
+++ code/src/t02_ordering/demos/de01/NetworkConnection.java
@@ -1,80 +1,28 @@
 package t02_ordering.demos.de01;
 
-
-/*
- Represents network connection. It is IMMUTABLE.
- */
-public class NetworkConnection //implements Comparable<NetworkConnection>
+public class NetworkConnection
 {
-    /*
-    @Override
-    public int compareTo(NetworkConnection o) {
+    private ProtocolType protocolType;                      //value type
+    private TransportProtocolType transportProtocolType;    //value type  e.g. 1 for UDP
+    private float pingMS;                                   //value type e.g. 34.5
+    private IPAddress ipAddress;                            //reference type
 
-       // return Float.compare(this.pingMs, o.pingMs);
-
-        float delta = this.pingMs - o.getPingMs();
-        if(delta == 0)
-            return 0;
-        else if (delta > 0)
-            return 1;
-        else
-            return -1;
-    }
-*/
-
-    //region Fields
-    private String ipAddress;
-    private int port;
-    private String protocol;  //REFACTOR
-    private float pingMs;
-    //endregion
-
-    //region Accessors
-    public float getPingMs() {
-        return pingMs;
-    }
-
-    public String getProtocol() {
-        return protocol;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public String getIpAddress() {
-        return ipAddress;
-    }
-    //endregion
-
-    //region Constructors
-    public NetworkConnection(String ipAddress, int port, String protocol) {
+    public NetworkConnection(ProtocolType protocolType, IPAddress ipAddress,
+                             TransportProtocolType transportProtocolType) {
+        this.protocolType = protocolType;
         this.ipAddress = ipAddress;
-        this.port = port;
-        this.protocol = protocol;
+        this.transportProtocolType = transportProtocolType;
     }
-    //endregion
 
-    //region Core
-    public void ping()
-    {
-        this.pingMs = Math.round(Math.random() * (100-10) + 10);
-    }
-    //endregion
-
-
-    //region Housekeeping
-    //Annotations start with @
     @Override
     public String toString() {
         return "NetworkConnection{" +
-                "ipAddress='" + ipAddress + '\'' +
-                ", port=" + port +
-                ", protocol='" + protocol + '\'' +
-                ", pingMs=" + pingMs +
+                "protocolType=" + protocolType +
+                ", transportProtocolType=" + transportProtocolType +
+                ", pingMS=" + pingMS +
+                ", ipAddress=" + ipAddress +
                 '}';
     }
 
 
-    //endregion
 }
