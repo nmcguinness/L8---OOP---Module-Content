c40ec6b06df48754021f950ae1c43c731df5fcc0
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-11-03 11:05:56 +0000

    :memo: Minor changes to to2 and t03

--- code/src/t02_ordering/demos/de01/Demo.java
+++ code/src/t02_ordering/demos/de01/Demo.java
@@ -53,54 +53,8 @@ public class Demo {
                 .thenComparing(IPAddress::getIpC)
                 .thenComparing(IPAddress::getIpD));
 
-        //interlude for a better print
+        //using lambdas for a better print (See topic 3 notes)
         ipList.forEach((IPAddress ip) -> System.out.println(ip));
-
-//        for(IPAddress ip : ipList)
-//            System.out.println(ip);
-//
-//        for(int i = 0; i < ipList.size(); i++)
-//            System.out.println(ipList.get(i));
-
-//        //ordering = sorting (one or more fields)
-//        ArrayList<NetworkConnection> netList = new ArrayList<>(
-//          List.of(
-//                  new NetworkConnection("A", 8080, "HTTPS"),
-//                  new NetworkConnection("Z", 20, "FTP"),
-//                  new NetworkConnection("R", 114, "SMTP"),
-//                  new NetworkConnection("Q", 80, "HTTP"),
-//                  new NetworkConnection("B", 21, "FTP")
-//          )
-//        );
-//
-//        //unsorted
-//        System.out.println("Unsorted...");
-//        for(NetworkConnection c: netList)
-//            System.out.println(c);
-//
-//        //sort by port comparator defined as external comparator
-//        System.out.println("Port: sort by external comparator...");
-//        var portComp = new PortComparator(-1);
-//        netList.sort(portComp);
-//        for(NetworkConnection c: netList)
-//            System.out.println(c);
-//
-//        //sort by port comparator defined as an anonymous comparator
-//        System.out.println("Port: sort by external comparator as an anonymous comparator...");
-//        netList.sort(new Comparator<NetworkConnection>() {
-//            @Override
-//            public int compare(NetworkConnection o1, NetworkConnection o2) {
-//                return Float.compare(o1.getPort(), o2.getPort());
-//            }
-//        });
-//        for(NetworkConnection c: netList)
-//            System.out.println(c);
-//
-//        //sort by port comparator defined as a lambda expression
-//        System.out.println("Port: sort by external comparator as lambda expression...");
-//        netList.sort((NetworkConnection a, NetworkConnection b) -> Float.compare(a.getPort(), b.getPort()));
-//        for(NetworkConnection c: netList)
-//            System.out.println(c);
     }
 
     public static int mySorter(IPAddress a, IPAddress b)
