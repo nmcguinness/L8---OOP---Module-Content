8bff55cc3b3e4b53288a24a26b12ce027bf2402f
Niall McGuinness <niall.mcguinness@dkit.ie>
2025-11-20 12:23:00 +0000

    Update Grenade.java

--- code/src/t03_arraylist/demos/de03/Grenade.java
+++ code/src/t03_arraylist/demos/de03/Grenade.java
@@ -1,6 +1,44 @@
 package t03_arraylist.demos.de03;
 
-public class Grenade {
-    //2-3 fields that make a grenade unique
-    //see if you can use an enum
+public enum ExplosiveType{
+    Frag, Incendiary, Smoke, Flashbang
+}
+public class Grenade extends Weapon{
+    private ExplosiveType explosiveType;
+    private float range;
+
+    public ExplosiveType getExplosiveType() {
+        return explosiveType;
+    }
+
+    public void setExplosiveType(ExplosiveType explosiveType) {
+        this.explosiveType = explosiveType;
+    }
+
+    public float getRange() {
+        return range;
+    }
+
+    public void setRange(float range) {
+        this.range = range;
+    }
+
+    public Grenade(String make, String model, float calibre, int clipSize, float cost, float weight, ExplosiveType explosiveType, float range) {
+        this(make, model, calibre, clipSize, cost,
+                weight, false, explosiveType, range);
+    }
+
+    public Grenade(String make, String model, float calibre, int clipSize, float cost, float weight, boolean isOnceOnly, ExplosiveType explosiveType, float range) {
+        super(make, model, calibre, clipSize, cost, weight, isOnceOnly);
+        this.explosiveType = explosiveType;
+        this.range = range;
+    }
+
+    @Override
+    public String toString() {
+        return "Grenade{" +
+                "explosiveType=" + explosiveType +
+                ", range=" + range +
+                '}';
+    }
 }
